5b6154836207e870968f6f7d0897bd18c32a5b73
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2024-05-23 11:12:14 -0400

    Update employee availability endpoint to get employee info from JWT

--- backend/src/main/java/shift/scheduler/backend/controller/EmployeeController.java
+++ backend/src/main/java/shift/scheduler/backend/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package shift.scheduler.backend.controller;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shift.scheduler.backend.model.Availability;
 import shift.scheduler.backend.model.Employee;
+import shift.scheduler.backend.model.Manager;
 import shift.scheduler.backend.repository.AvailabilityRepository;
 import shift.scheduler.backend.repository.EmployeeRepository;
+import shift.scheduler.backend.service.EmployeeService;
+import shift.scheduler.backend.service.JwtService;
 
 import java.util.Collection;
 
@@ -18,16 +22,19 @@ import java.util.Collection;
 public class EmployeeController {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private JwtService jwtService;
+
+    @Autowired
+    private EmployeeService employeeService;
 
     @Autowired
     private AvailabilityRepository availabilityRepository;
 
-    @PostMapping(value = "/{username}/availability", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> newAvailabilities(@RequestBody Collection<Availability> availabilities,
-                                                  @PathVariable String username) {
+    @PostMapping(value = "/availability", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> updateAvailabilities(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader,
+                                                       @RequestBody Collection<Availability> availabilities) {
 
-        Employee employee = employeeRepository.findByAccountUsername(username).orElse(null);
+        Employee employee = getEmployee(authHeader);
 
         if (employee == null)
             return ResponseEntity.badRequest().body("Employee does not exist");
@@ -37,11 +44,18 @@ public class EmployeeController {
         try {
             availabilityRepository.saveAll(availabilities);
         } catch (ConstraintViolationException e) {
-            return ResponseEntity.badRequest().body("Invalid details");
+            return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body("Internal server error");
         }
 
         return ResponseEntity.ok("Availabilities added");
+    }
+
+    private Employee getEmployee(String authHeader) {
+
+        String token = jwtService.extractTokenFromHeader(authHeader);
+        String username = jwtService.extractUsername(token);
+        return (Employee) employeeService.findByUsername(username);
     }
 }
