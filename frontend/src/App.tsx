56684e1242f3f399831dad78233628cb9c109859
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2024-06-02 13:10:33 -0400

    Set up React router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, RouterProvider } from "react-router-dom"
 import NavBar from "./components/NavBar"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
       <NavBar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
