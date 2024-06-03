9150de3414c709c014c1be1b7522a257bd177739
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2024-06-03 16:31:54 -0400

    Add CORS configuration to backend

--- backend/src/main/java/shift/scheduler/backend/config/WebSecurityConfig.java
+++ backend/src/main/java/shift/scheduler/backend/config/WebSecurityConfig.java
@@ -14,9 +14,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import shift.scheduler.backend.config.filter.JwtAuthenticationFilter;
 import shift.scheduler.backend.model.Role;
 import shift.scheduler.backend.repository.AccountRepository;
+
+import java.util.Arrays;
 
 /**
  * The application's security configurations. Based on
@@ -50,11 +57,26 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        // TODO: Modify allowed origins
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 // TODO: Add custom CSRF configuration
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> {
                     authorize
                             .requestMatchers("/user/**").permitAll()
